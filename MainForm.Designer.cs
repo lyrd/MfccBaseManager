2f84aa5d39b726736055f08bfa49e002b5502a14
lyrd <antonmol-92@yandex.ru>
2015-05-25 22:32:35 +0800

    v.0.0.0.1

---
+++ MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace MfccBaseManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tBPath = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tBPath
+            // 
+            this.tBPath.BackColor = System.Drawing.SystemColors.Window;
+            this.tBPath.Location = new System.Drawing.Point(6, 19);
+            this.tBPath.Name = "tBPath";
+            this.tBPath.ReadOnly = true;
+            this.tBPath.Size = new System.Drawing.Size(349, 20);
+            this.tBPath.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(361, 17);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Открыть";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tBPath);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(442, 53);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Добавление слова в базу";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 77);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(482, 116);
+            this.MinimumSize = new System.Drawing.Size(482, 116);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MFCC Base Manager";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBPath;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OperationWithFiles;
+using System.IO;
+
+namespace MfccBaseManager
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private readonly string settingsFilePath = Directory.GetCurrentDirectory() + "\\Settings.ini";
+        private string pathToBase = "";
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StreamReader streamReader = new StreamReader(pathToBase, Encoding.UTF8);
+
+            string[] text;
+
+            while (true)
+            {
+                string temp = streamReader.ReadLine();
+
+                if (temp == null) break;
+
+                text = temp.Split(';');
+
+                MessageBox.Show(temp);
+                MessageBox.Show(text[0] + "\r\n" + text[1]);
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            using (IniFile ini = new IniFile(settingsFilePath))
+            {
+                pathToBase = ini.IniReadValue("PathToBase", "Path");
+            }
+        }
+    }
+}
