529eac4f919b9bf5e29822ea179a53d378794958
lyrd <antonmol-92@yandex.ru>
2015-05-26 17:13:33 +0800

    v.0.0.0.2

---
+++ Classes/Constants.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleCreator
+{
+    static class Constants
+    {
+        //Коэффициенты окга Хэмминга
+        /// <summary>
+        ///  alpha = 0,543478260869565
+        /// </summary>
+        public static readonly double alpha = 25d / 46d; //or 0.54 or 0.53836
+        /// <summary>
+        ///  beta = 0,456521739130435
+        /// </summary>
+        public static readonly double beta = (1d - alpha); //or 0.46 or 0.46164  //public static readonly decimal betta = (1m - alpha) / 2m;
+
+        //Длина фрейма (миллисекунды)
+        public static readonly ushort frameLenght = 256;//50 byte 256
+
+        //Процент наложения фреймов (0 <= x < 1)
+        public static readonly float frameOverlap = 0.5F;
+
+        //Минимальный размер слова (во фреймах)
+        //В базе слов - avg 500 мс.
+        //Пусть - min 200 мс.
+        public static readonly short wordMinSize = (short)((200 / frameLenght) / (1 - frameOverlap));//byte
+
+        //Минимальное количество фреймов между двумя словами.
+        //Пусть минимальное расстояние между двумя словами составляет 50% от минимального размера слова 
+        public static readonly short wordMinDistance = (short)(wordMinSize * 0.5F); //byte
+
+        ///Количество MFCC коэффициетов
+        public static readonly byte mfccSize = 12;//12 10
+
+        ///Диапазон частот
+        public static readonly short mfccFreqMin = 300;
+        public static readonly short mfccFreqMax = 8000;//4000 8000
+
+        //Количество значений
+        public static readonly byte entropyBins = 75;
+        //Порог энтропии
+        public static readonly double entropyThreshold = 0.1;//0.1
+
+        public static readonly uint sampleRate = 44100;
+
+        /// <summary>
+        /// epsilon = 2,22044604925031E-16
+        /// </summary>
+        public static double Epsilon()
+        {
+            double epsilon = 1d;
+            double tmp = 1d;
+            while ((1d + (tmp /= 2d)) != 1d) epsilon = tmp;
+            return epsilon;
+        }
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OperationWithFiles;
 using System.IO;
+using SubtitleCreator;
 
 namespace MfccBaseManager
 {
@@ -19,35 +20,100 @@ namespace MfccBaseManager
             InitializeComponent();
         }
 
-        private readonly string settingsFilePath = Directory.GetCurrentDirectory() + "\\Settings.ini";
-        private string pathToBase = "";
+        private string pathToBase = "mfccBase.mfcc";
+        private Dictionary<string, double[]> samplesMFCC = new Dictionary<string, double[]>();
+        private string word = "";
+        private double[] mfcc;
+        private string audioFile = "";
+
+        ErrorProvider errorProvider = new ErrorProvider();
+
+        private string Transliteration(string source)
+        {
+            Dictionary<string, string> letters = new Dictionary<string, string>();
+
+            string[,] setOfLetters ={ { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я" },
+                                    { "a", "b", "v", "g", "d", "e", "yo", "zh", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "c", "ch", "sh", "sch", "j", "i", "j", "e", "yu", "ya", "A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "H", "C", "Ch", "Sh", "Sch", "J", "I", "J", "E", "Yu", "Ya" }};
+
+            for (byte i = 0; i < setOfLetters.GetLength(1); i++)
+                letters.Add(setOfLetters[0, i], setOfLetters[1, i]);
+
+            foreach (KeyValuePair<string, string> pair in letters)
+            {
+                source = source.Replace(pair.Key, pair.Value);
+            }
+
+            return source;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            word = tBWord.Text;
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Wav Files (*.wav)|*.wav";
+            audioFile = tBPath.Text = dialog.ShowDialog() == DialogResult.OK ? dialog.FileName : "err";           
+        }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            StreamReader streamReader = new StreamReader(pathToBase, Encoding.UTF8);
-
-            string[] text;
-
-            while (true)
+            if (tBPath.Text != "" & tBWord.Text != "")
             {
-                string temp = streamReader.ReadLine();
+                try
+                {
+                    WavData.ReadWavDataChunk(audioFile);
+                }
+                catch (WavException ex)
+                {
+                    MessageBox.Show(String.Format(ex.Message), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
 
-                if (temp == null) break;
+                double[] rawdata = WavData.NornalizeData;
+                mfcc = MFCC.Transform(ref rawdata, 0, WavData.SampleNumber, Constants.mfccSize, Constants.sampleRate, Constants.mfccFreqMin, Constants.mfccFreqMax);
 
-                text = temp.Split(';');
+                string mfccString = "";
+                for (int i = 0; i < mfcc.Length; i++)
+                    mfccString += mfcc[i] + "/";
+                mfccString = mfccString.Substring(0, mfccString.Length - 1);
 
-                MessageBox.Show(temp);
-                MessageBox.Show(text[0] + "\r\n" + text[1]);
+                using (StreamWriter streamwriter = new StreamWriter(pathToBase, true, Encoding.UTF8))
+                {
+                    streamwriter.WriteLine(String.Format("{0};{1}", word, mfccString));
+                }
             }
-
+            else
+            {
+                if (tBPath.Text == "") { errorProvider.SetError(tBPath, "Обязательно для заполнения"); }
+                if (tBWord.Text == "") { errorProvider.SetError(tBWord, "Обязательно для заполнения"); }
+                if (tBPath.Text == "err") { errorProvider.SetError(tBPath, "Путь не выбран"); }
+            }
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        private void ReadFromDataBase(ref Dictionary<string, double[]> samplesMFCC, string pathToBase)
         {
-            using (IniFile ini = new IniFile(settingsFilePath))
+            using (StreamReader streamReader = new StreamReader(pathToBase, Encoding.UTF8))
             {
-                pathToBase = ini.IniReadValue("PathToBase", "Path");
-            }
+                while (true)
+                {
+                    string[] line = new string[2];
+                    string[] array = new string[12];
+                    double[] mfccs = new double[12];
+
+                    string temp = streamReader.ReadLine();
+
+                    if (temp == null) break;
+
+                    line = temp.Split(';');
+                    array = line[1].Split('/');
+
+                    for (int i = 0; i < array.Length; i++)
+                        Double.TryParse(array[i], out mfccs[i]);
+
+                    samplesMFCC.Add(line[0], mfccs);
+                }
+            }          
         }
+
+
     }
 }
